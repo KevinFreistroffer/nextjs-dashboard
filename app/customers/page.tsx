75711b23fd71b689c7b16b34049330efe8188b64
Kevin Freistroffer <Kevin.freistroffer@gmail.com>
2024-05-27 18:17:28 -0700

    first

---
+++ app/customers/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CustomersPage = (): JSX.Element => {
+  return (
+    <div>
+      <h1>Customers Page</h1>
+      {/* Add your page content here */}
+    </div>
+  );
+};
+
+export default CustomersPage;
